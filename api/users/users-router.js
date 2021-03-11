569f4081b101958fa7843db4d48fad91242c35dc
Vince Garcia <vinceryangarcia@gmail.com>
2021-03-10 20:52:28 -0800

    server functional

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,12 +2,25 @@ const express = require('express');
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
+const Users = require('./users-model');
+const Posts = require('../posts/posts-model');
+const Middleware = require ('../middleware/middleware')
+
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
-});
+  Users.get()
+  .then(users=>{
+    res.status(200).json({users})
+  })
+  .catch(()=>{
+    res.status(500).json({
+      error: "Couldn't fetch users."
+    })
+  }
+);
 
 router.get('/:id', (req, res) => {
   // RETURN THE USER OBJECT
