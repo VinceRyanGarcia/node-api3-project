8044509b5c93be6c43c8740c609fe10a450cea6a
Vince Garcia <vinceryangarcia@gmail.com>
2021-03-10 22:48:59 -0800

    mvp-ish

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require your server and launch it
 const server = require('./api/server');
 
-const port = 5000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => 
 console.log(`Port ${port} is working. Fucking sick. Shout out to my homies at Lady Footlocker`)
